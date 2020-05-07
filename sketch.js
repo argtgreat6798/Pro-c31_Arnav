2d15716e15709ed7fe3b3fe0201b142d3d10ec81
argtgreat7698 <62493766+argtgreat6798@users.noreply.github.com>
2020-05-07 16:52:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+// Daniel Shiffman
+// http://youtube.com/thecodingtrain
+// http://codingtra.in
+
+// Coding Challenge #115: Snake Game Redux
+// https://youtu.be/OMoVcohRgZA
+
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+
+function setup() {
+  createCanvas(400, 400);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5);
+  snake = new Snake();
+  foodLocation();
+}
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+    snake.setDir(0, -1);
+  } else if (key == ' ') {
+    snake.grow();
+  }
+
+}
+
+function draw() { 
+  noStroke();
+  textSize(5);
+  stroke(255,255,255);
+  strokeWeight(4);
+  text("remember this game? Well now, its children's turn to play it too.",200,200);
+
+  scale(rez);
+  background(3,29,55);
+  if (snake.eat(food)) {
+    foodLocation();
+  }
+  snake.update();
+  snake.show();
+
+
+  if (snake.endGame()) {
+    print("END GAME");
+    background(255, 0, 0);
+    text("BYEBYE😈",210,200);
+    stroke(255,255,255);
+    noLoop();
+  }
+
+  noStroke();
+  fill(255, 0, 0);
+  rect(food.x, food.y, 1, 1);
+}
